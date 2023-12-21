ec02549e88b6b64cb940893e8303e49fd1f787c8
philips777 <mymailforall@ukr.net>
2023-12-21 21:03:07 +0200

    Fix task 1

--- Task_1.py
+++ Task_1.py
@@ -1,13 +1,13 @@
 from queue import Queue
 import random
-
+import uuid
 
 queue = Queue()
 
 
 def generate_request():
 
-    request = {"id": str(random.randint(1, 1000)),
+    request = {"id": str(uuid.uuid4()),
                "data": str(random.randint(1, 100000))}
     queue.put(request)
 
@@ -15,16 +15,20 @@ def generate_request():
 def process_request():
     if not queue.empty():
         request = queue.get()
-        print(f"Обробка заявки id {request['id']} даними: {request['data']}")
+        print(f"Обробка заявки id {request['id']} з даними: {request['data']}")
 
     else:
         print("Черга порожня")
 
 
-while True:
-    generate_request()
-    process_request()
+try:
 
-    user_input = input("Для виходу введіть 'exit': ")
-    if user_input.lower() == 'exit':
-        break
+    while True:
+        generate_request()
+        process_request()
+
+
+except KeyboardInterrupt:
+    print("Програму завершено")
+
+# Завершити Ctrl+C
